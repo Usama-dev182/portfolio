99174166e0e8cad73a5a43a54cbbd346175dc7e0
Usama-dev182 <infousamaali2@gmail.com>
2023-10-31 03:00:33 -0700

    changes nex

--- next.config.js
+++ next.config.js
@@ -13,24 +13,9 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true
   },
-  images: {
-    domains: ['dev.ptechfusion.com']
-  },
+  
 
-  // distDir: "build",
-  // exportPathMap: {
-  //   '/': 'app/page.tsx',
-  //   '/about': '/app/about/page.tsx',
-  //   '/blog': 'app/blog/page.tsx',
-  //   '/blog-details ': '/app/blog-details/page.tsx',
-  //   '/blog-sidebar': '/app/blog-sidebar/page.tsx',
-  //   '/contact': '/app/contact/page.tsx',
-  //   '/error': '/app/error/page.tsx',
-  //   '/signin': '/app/signin/page.tsx',
-  //   '/signup': '/app/signup/page.tsx',
-  // },
 
-  // output: 'export',
 
 };
 
