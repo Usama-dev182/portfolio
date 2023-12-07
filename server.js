02c779ad01e46d3219d2440162694262fb64e9bb
Usama-dev182 <infousamaali2@gmail.com>
2023-12-07 03:30:02 -0800

    upd

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { parse } = require('url')
 const next = require('next')
  
 const dev = process.env.NODE_ENV !== 'production'
-const hostname = process.env.NODE_ENV !== "production" ?  'localhost' : 'https://test.ptechfusion.com'
+const hostname = process.env.NODE_ENV !== "production" ?  'localhost' : 'https://newdev.ptechfusion.com'
 const port = process.env.PORT || 3000
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port })
